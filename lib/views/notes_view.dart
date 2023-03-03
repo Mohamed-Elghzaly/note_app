a3f2adf0ab0a71dcc446ada50f53f09f8b1340c0
Mohamed-Elghzaly <mohamedelghzaly001@gmail.com>
2023-03-03 16:21:44 +0200

    create custom app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          const NotesViewBody(),
+        ],
+      ),
+    );
   }
 }
